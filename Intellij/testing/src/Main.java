d6304184d33e1154831b685d0898cc3e0d3a5569
Todd Pickell <toddpickell@me.com>
2012-08-30 09:26:51 -0500

    added eclipse and IntelliJ folders for some java tutorial work

---
+++ Intellij/testing/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: toddpickell
+ * Date: 8/29/12
+ * Time: 11:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
