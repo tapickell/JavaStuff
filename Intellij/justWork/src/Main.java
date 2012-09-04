3ee671e4e7a3749e1f2f39547456b8a3e757dd95
myappleguy <myappleguy@gmail.com>
2012-09-04 11:28:31 -0500

    stuff

---
+++ Intellij/justWork/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: toddpickell
+ * Date: 9/4/12
+ * Time: 8:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
